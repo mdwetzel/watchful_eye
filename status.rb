9a091a27a66fc6f1e0107eb822a6377276b02bad
Mark Wetzel <mark@markwetzel.com>
2013-10-12 23:59:10 -0400

    Moved logic.

--- status.rb
+++ status.rb
@@ -2,11 +2,24 @@ require 'net/http'
 require 'sinatra'
 
 get '/' do
-  res = Net::HTTP.get_response(URI.parse('http://www.google.com'))
+  erb :home
+end
+
+post '/' do
+  unless params[:url].empty?
+    url = params[:url]
+    unless url =~ /^http:\/\//
+      url = "http://#{url}"
+    end
+    
+    res = Net::HTTP.get_response(URI.parse(url))
   
-  if res.code =~ /2|3\d{2}/
-    @status = "Everything looks normal."
-  else
-    @status = "The server appears to be down!"  
+    if res.code =~ /2|3\d{2}/
+      @status = "#{url} seems to be working okay."
+    else
+      @status = "#{url} appears to be down!"  
+    end
   end
+  
+  erb :home
 end
